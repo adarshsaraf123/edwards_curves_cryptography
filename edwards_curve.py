c29826f2bf16cce8678ccb742fac7e4f25cdc0c6
Adarsh Saraf <adarshsaraf123@gmail.com>
2014-11-08 11:56:05 +0530

    to implement the edwards curve library; work done till September 2014

---
+++ edwards_curve.py
@@ -0,0 +1,84 @@
+#from sage.schemes.elliptic_curves.ell_rational_field import EllipticCurve_rational_field
+#from sage.schemes.elliptic_curves.ell_field import EllipticCurve_field
+import sage.schemes.projective.projective_space as projective_space
+from sage.all import QQ
+import sage.schemes.plane_curves.projective_curve as plane_curve
+from sage.schemes.elliptic_curves.edwards_curve_point import EdwardsCurvePoint
+import sage.rings.all as rings
+from sage.structure.sequence import Sequence
+from sage.rings.arith import is_square
+from sage.schemes.elliptic_curves.constructor import EllipticCurve
+#Q = RationalField()
+
+class EdwardsCurve(plane_curve.ProjectiveCurve_generic):
+    Element = EdwardsCurvePoint
+    def __init__(self, d, extra = None):
+        if extra != None:
+            K, self.__d = d, extra
+        else: 
+            if isinstance(d, (rings.Rational, rings.Integer, int, long)):
+                K = QQ
+            else:
+                K = d.parent()
+            self.__d = d
+        self.__d = K(self.__d)
+        if self.__d.is_zero() or self.__d.is_one():
+            raise ValueError("An Edwards Curve cannot have d as 1 or 0 of the base field")
+        if is_square(self.__d):
+            raise ValueError("An Edwards Curve cannot have d which is a square in the base field")
+        if K.characteristic() == 2:
+            raise ValueError("the base field cannot have characteristic two")
+        self.__base_ring = K
+        PP = projective_space.ProjectiveSpace(2, K, names='xyz');
+        x, y, z = PP.coordinate_ring().gens()
+        self.f = x**2 * z**2 + y**2 * z**2 - z**4 - self.__d * x**2 * y**2
+        plane_curve.ProjectiveCurve_generic.__init__(self, PP, self.f)
+        # super(MyEdwardsCurve,self).__init__([5,6])
+        #print self.__A
+    def _repr_(self):
+        return "Edwards curve defined by the equation %s over %s" %(self.f, self.base_ring())
+    def get_d(self):
+        return self.__d
+    def base_ring(self):
+        return self.__base_ring
+    def __call__(self, *args, **kwgs):
+        return EdwardsCurvePoint(self, args[0])
+    def points(self):
+        try:
+            return self.__points
+        except AttributeError: pass
+        
+        if not self.base_ring().is_finite():
+            raise TypeError("points() is defined only for Edwards curve over finite fields")
+        v = []
+        for x in self.base_ring():
+            for y in self.base_ring():
+                try:
+                    v.append(self([x,y,1]))
+                except (TypeError,ValueError):
+                    pass
+        v.sort()
+        self.__points = Sequence(v,immutable = True)
+        return self.__points
+    def torsion_points(self, n):
+        torsion_points_list = []
+        for p in self.points():
+            if n % p.order() == 0:
+                torsion_points_list.append(p)
+        return torsion_points_list
+    def weierstrass_curve(self):
+        try:
+            return self.__weierstrass_curve
+        except AttributeError:
+            pass
+        d = self.__d
+        self.__weierstrass_curve = EllipticCurve(self.base_ring(), [0, -(d+1), 0, -4*d, 4*d*(d+1)])
+        return self.__weierstrass_curve    
+        
+    def zero(self):
+        return self(0)
+    
+        
+    
+        
+        
---
+++ edwards_curve_point.py
@@ -0,0 +1,78 @@
+from sage.schemes.projective.projective_point import SchemeMorphism_point_abelian_variety_field
+from sage.rings.integer import Integer
+class EdwardsCurvePoint(SchemeMorphism_point_abelian_variety_field):
+    def __init__(self, curve, v, check = True):
+        point_homset = curve.point_homset()
+        if v == 0:
+            v = (0, 1, 1) 
+        SchemeMorphism_point_abelian_variety_field.__init__(self, point_homset, v, check=True)
+    def _add_(self, right):
+        if self.is_zero():
+            return right
+        if right.is_zero():
+            return self
+        E = self.curve()
+        d = E.get_d()
+        x1, y1 = self[0], self[1]
+        if self == right:
+            x3 = (2*x1*y1)/(x1**2 + y1**2)
+            y3 = (y1**2 - x1**2)/(2 - x1**2 - y1**2)
+        else:
+            x2, y2 = right[0], right[1]
+            temp = d*x1*y1*x2*y2
+            x3 = (x1*y2 + y1*x2)/(1+temp)
+            y3 = (y1*y2 - x1*x2)/(1-temp) 
+        return E([x3, y3, 1], check=False)
+    def _lmul_(self, c):
+        Q = self.curve()(0)
+        bin_c = Integer(c).binary() #take mod(order(self)); taking care of negative c
+        for i in range(len(bin_c)):
+            Q = Q+Q
+            if bin_c[i] == '1':
+                Q += self
+        return Q
+    def __nonzero__(self):
+        return not(( not bool(self[0]) ) and (self[1] == self[2]) )
+    def curve(self):
+        return self.scheme()
+    def additive_order(self):
+        try:
+            return self._order
+        except AttributeError: pass
+             
+        Q = self.curve().zero()
+        i = 1
+        while(1):
+            Q += self
+            if Q.is_zero():
+                self._order = i
+                break
+            i += 1
+        return self._order
+    def weierstrass_point(self):
+        try:
+            return self.__weierstrass_point
+        except AttributeError:
+            pass
+        E = self.curve()
+        u = self[0]
+        v = self[1]
+        d = E.get_d()
+        w = (d*u*u - 1)*v
+        x = ( -2 * (w-1) ) / (u*u)
+        y = ( 4*(w-1) + 2*(d+1)*u*u ) / (u**3)
+        self.__weierstrass_point = E.weierstrass_curve()([x,y])
+        return self.__weierstrass_point
+    #===========================================================================
+    # def __init__(self,curve,v,check = False):
+    #     point_homset = curve.point_homset()
+    #     if is_SchemeMorphism(v) or isinstance(v, EllipticCurvePoint_field):
+    #         v = list(v)
+    #     elif v == 0:
+    #         # some of the code assumes that E(0) has integral entries
+    #         # irregardless of the base ring...
+    #         #R = self.base_ring()
+    #         #v = (R.zero(),R.one(),R.zero())
+    #         v = (0, 1, 0)
+    #     SchemeMorphism_point_abelian_variety_field.__init__(self, point_homset, v, check=False)
+    #===========================================================================
